9f9e8d478539834c90ae2d44e94a3e42e8443a07
FedericoIbarra <is702981@iteso.mx>
2020-04-04 17:02:39 -0600

    About us page

--- front-iot/src/components/AboutUs.vue
+++ front-iot/src/components/AboutUs.vue
@@ -1,6 +1,34 @@
 <template>
   <div class="hello">
-    <h3>Aquí va una poderosa foto de nosotros</h3>
+    <h2>Quienes Somos</h2>
+
+    <b-container fluid>
+      <b-row>
+        <b-col lg="4">
+          <div class="cont">
+            <img alt="Choya logo" class="imgf" src="../assets/Fede.jpg">
+          </div>
+          <h3>Federico Ibarra</h3>
+          <h5>Software Lead</h5>
+        </b-col>
+
+        <b-col lg="4">
+          <div class="cont">
+            <img alt="Choya logo" class="imgd" src="../assets/Diego.jpeg">
+          </div>
+          <h3>Diego Espinoza</h3>
+          <h5>Experience Lead</h5>
+        </b-col>
+
+        <b-col lg="4">
+          <div class="cont">
+            <img alt="Choya logo" class="imgi" src="../assets/Isaac.jpeg">
+          </div>
+          <h3>Isaac Vázquez</h3>
+          <h5>Hardware Lead</h5>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
@@ -18,19 +46,44 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+h2 {
+  padding: 2em 0 2em 0;
+  color: #262626;
+}
+
+h3{
+  padding: 2em 0 1em 0;
+  color: #262626;
+}
+
+h5{
+  padding: 0 0 6em 0;
+  color: #262626;
+}
+
+.cont{
+  width:15em; /*container-width*/
+  height: 15em;
+  border-radius: 50%;
+  overflow:hidden; /*hide bounds of image */
+  margin: auto;
 
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.imgf{
+  height: 100%;
+  margin-left: -6em;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.imgd{
+  width: 100%;
+  margin-top: -2em;
+  margin-left: -1em;
 }
-a {
-  color: #42b983;
+
+.imgi{
+  width: 100%;
+  margin-top: -4em;
+  margin-left: 0em;
 }
 </style>
